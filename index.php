bc4cf3032abc4d6efe79f3d6704dabd2771caaf1
Nelly <nelly.quesada@orange.fr>
2017-06-28 18:36:39 +0200

    Ajout des fichiers index.txt et index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<div class="page">
+
+        <section>
+            <article id="accueil">
+
+                    <div id="bienvenue">
+
+                        <h1> Bienvenue sur le site culturel et sportif de la ville de Levet </h1>
+
+                        <p>
+                            Vous trouverez sur ce site : les actualités culturelles et sportives de la ville de Levet, des détails concernant les activités proposées par la ville, les activités du mois auxquelles vous pourrez vous inscrire d'un simple clique ainsi qu'un formulaire de contact pour toutes questions éventuelles.
+                        </p>
+                        <p id="parcourir">
+                            N’hésitez pas à parcourir le site sans plus attendre...
+                        </p>
+                    </div>
+
+                    <div id="pictos">
+                        <a href = "actualites.html"> <img src="../images/picto_actualites.png" class="picto" alt="picto actualites" /> </a>
+                        <a href = "plusdinfos.html"> <img src="../images/picto_infos.png" class="picto" alt="picto plus d'informations" /> </a>
+                        <a href = "activites.html"> <img src="../images/picto_activites.png" class="picto" alt="picto activites du mois" /> </a>
+                        <a href = "contact.html"> <img src="../images/picto_contact.png" class="picto" alt="picto contact" /> </a>
+                    </div>
+
+                </article>
+
+        </section>
+
+        <?php get_sidebar(); ?>
+</div> 
+
+<?php get_footer(); ?>
+
+        
+
+           
